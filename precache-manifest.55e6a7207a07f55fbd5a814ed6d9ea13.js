bb40dc7dbb46b93a7cedf07decbb8a67e7063644
tandemungwa <tmungwa@gmail.com>
2019-08-04 20:36:04 -0400

    Updates

---
+++ precache-manifest.55e6a7207a07f55fbd5a814ed6d9ea13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9fe9daf0b0f1b624f29f08779f2b88c",
+    "url": "/binge/index.html"
+  },
+  {
+    "revision": "29aa635ab2df0dd8c0d5",
+    "url": "/binge/static/css/2.22921dbe.chunk.css"
+  },
+  {
+    "revision": "c291afe5db057267924c",
+    "url": "/binge/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "29aa635ab2df0dd8c0d5",
+    "url": "/binge/static/js/2.91010634.chunk.js"
+  },
+  {
+    "revision": "c291afe5db057267924c",
+    "url": "/binge/static/js/main.a956e005.chunk.js"
+  },
+  {
+    "revision": "4dcfe97dc7a872d8283a",
+    "url": "/binge/static/js/runtime~main.852517d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/binge/static/media/logo.5d5d9eef.svg"
+  }
+]);
